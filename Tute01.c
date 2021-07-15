fb4ec34de5449c844445ae74dc638ea7d0b5b73c
M.C.M.A Sanjeevan <87358254+IT21023446@users.noreply.github.com>
2021-07-15 05:32:27 +0000

    I Completed Tute01

--- Tute01.c
+++ Tute01.c
@@ -4,8 +4,21 @@
 
 #include <stdio.h>
 
-int main() {
+int main(void) {
   
+  int mark1,mark2;
+  float avg;
+
+  printf("Enter mark for subject 1 :");
+  scanf("%d",&mark1);
+
+  printf("Enter mark for subject 2 :");
+  scanf("%d",&mark2);
+
+  avg=(mark1+mark2)/2;
+
+  printf("Average of %d and %d is : %.2f",mark1,mark2,avg);
+
   return 0;
 }
 
